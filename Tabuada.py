a1246277bdfc9165a3578f77d08ba71b991f411c
Pedro Hermano <pedro.hermano@gmail.com>
2022-02-10 22:15:53 -0300

    commit inicial

---
+++ Tabuada.py
@@ -0,0 +1,11 @@
+
+
+contador = 0
+tabuada = input('Qual o número da tabuada?')
+tabuada = int(tabuada)
+print('Tabuada do', tabuada, 'é:')
+while contador < 10:
+    contador = contador + 1
+    print(tabuada, 'x', contador, '=', tabuada * contador)
+
+print('fim da tabuada de', tabuada)
